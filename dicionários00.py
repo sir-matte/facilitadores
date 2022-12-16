08189bb97a9e254579b1e29e2576a32430009742
Gustavo Matte <gustavo.matte@hotmail.com>
2022-12-15 22:51:24 -0300

    dicas dicionário

---
+++ dicionários00.py
@@ -0,0 +1,12 @@
+user = {
+    'nome': 'user_name73',
+    'class': 'warrior',
+    'força': 97,
+    'vida': 100
+}
+
+
+x = user.get('nome', 0)
+xi = user.get('achievements', 'Não existe')
+print(x)
+print(xi)
