6bd56c982c19d7e53b23dc7f2731ff7d0121b8b3
Gustavo Matte <gustavo.matte@hotmail.com>
2022-12-18 18:38:34 -0300

    dicas listas

---
+++ listas01.py
@@ -0,0 +1,12 @@
+inventory = [
+    'Crimsom Sword',
+    'Great Helm',
+    'Leather Boots'
+]
+chest = [
+    'Health Potion',
+    'Mana Potion',
+    'Mop of Riches'
+]
+inventory.extend(chest)
+print(inventory)
